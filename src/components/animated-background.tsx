27538a836fe06ce2d1eeb67edb87af2ec2ad3176
Koustav Ghosh <k4ishyt94@gmail.com>
2025-11-08 08:43:14 +0000

    LIKE THIS - https://cdn.pixabay.com/video/2024/03/07/203336-920723750_la

--- src/components/animated-background.tsx
+++ src/components/animated-background.tsx
@@ -11,7 +11,7 @@ const AnimatedBackground = React.memo(() => {
         muted
         playsInline
         className="w-full h-full object-cover"
-        src="https://motionbgs.com/media/1085/plexus-blue.960x540.mp4"
+        src="https://cdn.pixabay.com/video/2024/03/07/203336-920723750_large.mp4"
       />
       <div className="absolute inset-0 w-full h-full bg-black/50" />
     </div>
